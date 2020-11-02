44965fd9c31506d4d5ce8b05cf7617e5b33d04de
Manish DEV <manish1771999@gmail.com>
2020-11-02 13:26:05 +0530

    Basic Structure Layout

---
+++ frontend/src/LandingPage/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LandingPage() {
+  return <div></div>;
+}
+
+export default LandingPage;
---
+++ frontend/src/Routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from "../LandingPage/LandingPage";
+
+function Home() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default Home;
