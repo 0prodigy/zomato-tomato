5bed920790943c40d442cfc30ee23efcb9741890
Manish DEV <manish1771999@gmail.com>
2020-11-03 10:58:51 +0530

    navigation menu added

--- frontend/src/LandingPage/Components/Navigation.jsx
+++ frontend/src/LandingPage/Components/Navigation.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
 import Signup from "./Signup";
 import LoginPage from "./LoginPage";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Card from "@material-ui/core/Card";
+import Fade from "@material-ui/core/Fade";
+
 const NavigationWrapper = styled.div`
   position: relative;
   padding: 0px;
@@ -51,6 +59,20 @@ const NavigationWrapper = styled.div`
         cursor: pointer;
       }
     }
+
+    .userDetails {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      > span {
+        margin-left: 8px;
+      }
+      button {
+        margin-left: auto;
+      }
+    }
   }
 
   @media only screen and (max-width: 769px) {
@@ -63,9 +85,47 @@ const NavigationWrapper = styled.div`
   }
 `;
 
+const useStyles = makeStyles((theme) => ({
+  expand: {
+    padding: 0,
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  root: {
+    position: "absolute",
+    borderRadius: 7,
+    top: 50,
+    width: 160,
+    height: "fit-content",
+    maxWidth: 180,
+    "& div": {
+      // fontWeight: 200,
+      height: 35,
+      padding: "5px",
+      paddingLeft: "12px",
+      "&:hover": {
+        backgroundColor: "#d1d1d1",
+      },
+    },
+  },
+}));
+
 function Navigation() {
+  const classes = useStyles();
   const [openLogin, setOpenLogin] = useState(false);
   const [openSignup, setOpenSignup] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   const handleNavigationClick = (e) => {
     const { name } = e.target;
@@ -89,16 +149,49 @@ function Navigation() {
         <ul className="mobileNavigation"></ul>
         <ul className="defaultNavigation">
           <li className="zomatoLogo">Get the app</li>
-          <li className="navigationButton">
-            <button name="login" onClick={handleNavigationClick}>
-              Login
-            </button>
-          </li>
-          <li className="navigationButton">
-            <button name="signup" onClick={handleNavigationClick}>
-              Signup
-            </button>
-          </li>
+          {authenticated ? (
+            <li className="navigationButton userDetails">
+              <Avatar>MK</Avatar>
+              <span>Manish</span>
+              <IconButton
+                disableRipple={true}
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+
+              <Fade in={expanded}>
+                <Card className={classes.root}>
+                  <div>Profile</div>
+                  <div>Notifications</div>
+                  <div>Bookmarks</div>
+                  <div>Reviews</div>
+                  <div>Network</div>
+                  <div>Find friends</div>
+                  <div>Settings</div>
+                  <div>Log out</div>
+                </Card>
+              </Fade>
+            </li>
+          ) : (
+            <>
+              <li className="navigationButton">
+                <button name="login" onClick={handleNavigationClick}>
+                  Login
+                </button>
+              </li>
+              <li className="navigationButton">
+                <button name="signup" onClick={handleNavigationClick}>
+                  Signup
+                </button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
 
