92ae97e43b2f91ddfa8ca17dcecd1cc40de254cb
Gargi-123 <dasgargi100@gmail.com>
2020-11-04 18:50:39 +0530

    restro page ui

--- frontend/src/Routes/Home.jsx
+++ frontend/src/Routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LandingPage from "../LandingPage/LandingPage";
+import RestroPage from "../RestroPage/RestroPage";
 
 function Home() {
   return (
     <div>
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <RestroPage />
     </div>
   );
 }
