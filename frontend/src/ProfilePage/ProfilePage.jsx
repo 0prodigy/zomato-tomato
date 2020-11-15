44c6f7dec0b22fe5aed5b383590082b07ea2042b
Gargi-123 <dasgargi100@gmail.com>
2020-11-15 15:41:10 +0530

    updated

--- frontend/src/ProfilePage/ProfilePage.jsx
+++ frontend/src/ProfilePage/ProfilePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import ProfileBody from "./Components/ProfileBody";
 import ProfileView from "./Components/ProfileView";
-import MainFooter from "../LandingPage/Components/MainFooter";
 
 function ProfilePage() {
   return (
@@ -10,7 +9,6 @@ function ProfilePage() {
       <Navbar />
       <ProfileView />
       <ProfileBody />
-      <MainFooter />
     </div>
   );
 }
